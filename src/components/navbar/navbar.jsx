a90cee4a87279d6689c8e56e272399670fa01c7d
Parasayer13 <paras.ayer13@gmail.com>
2023-06-14 17:23:56 +0545

    navbar_logo_text

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,9 +2,36 @@ import React from "react"
 import "./navbar.scss"
 
 const Navbar = () => {
+
+    const [active,setActive] = useState(false)
     return (
-        <div className='navbar'>Navbar</div>
-    )
-}
+        <div className={active ? "navbar active" : 'navbar'}>
+            <div className="container">
+                <div className="logo">
+                    {/* <Link to="/"> */}
+                        <span className='text'>Desixn</span>
+                    {/* </Link> */}
+                    <span className='dot'>.</span>
+                </div>
+                <div className="links">
+                    <span>Desixn Business</span>
+                    <span>Explore</span>
+                    <span>English</span>
+                    <span>Sign in</span>
+                    <span>Become a Seller</span>
+                    <button>Join</button>
+                </div>
+            </div>
+                <hr />
+                <div className="menu">
+                    <span>Test</span>
+                    <span>Test2</span>
+                    <span>Test3</span>
+                    <span>Test4</span>
+                    <span>Test5</span>
+                </div>
+        </div>
+    );
+};
 
 export default Navbar
